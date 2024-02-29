c714ff9ca7b0bd7917d28a30ae9c29026dcefca3
Nathan Nunes <nathannunessp587@gmail.com>
2024-02-29 08:47:39 -0300

    testes

---
+++ teste.py
@@ -0,0 +1,7 @@
+
+
+nome_dono = input('Digite o nome do dono do restaurante: ')
+nome_restaurante = input('Digite o nome do restaurante: ')
+endereco = input('Digite o endereço do restaurante: ')
+
+print(nome_dono, nome_restaurante, endereco)
