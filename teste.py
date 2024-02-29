9f10ddde8cf3c4072f9a428c81ec23811c841ed5
Nathan Nunes <nathannunessp587@gmail.com>
2024-02-29 10:39:48 -0300

    testes alura

--- teste.py
+++ teste.py
@@ -1,11 +1,32 @@
-nome = input('Digite seu nome: ')
-idade = input('Digite sua idade: ')
-pi_arredondado = 3.14
+#att 1
+#nome = input('Digite seu nome: ')
+#idade = input('Digite sua idade: ')
+#pi_arredondado = 3.14
 
-print('Python na Escola de Programação da Alura.')
+#print('Python na Escola de Programação da Alura.')
 
-print(f'Meu nome é {nome} e tenho {idade} anos')
+#print(f'Meu nome é {nome} e tenho {idade} anos')
 
-print('A\nL\nU\nR\nA')
+#print('A\nL\nU\nR\nA')
 
-print(f'O valor arredondado de pi é: {pi_arredondado}')
+#print(f'O valor arredondado de pi é: {pi_arredondado}')
+
+#att 2
+#num = int(input('Digite um numero de 1 ate 10: '))
+
+#if num % 2 == 0:
+    #print('O numero é par.')
+#else:
+   # print('O numero é impar.')
+
+#idade = int(input('Digite sua idade: '))
+
+#if(0 < idade <= 12):
+    #print('Você é criança!')
+#elif(12 < idade <= 18):
+#    print('Você é adolecente!')
+#elif(idade > 18):
+#    print('Você é adulto!')
+#else:
+#    print('Valor inválido!')
+
